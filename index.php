73f5721b576c73a81c174f173de431b33bc15ceb
Nicolas Barthès <nicolasbarthes.lana@gmail.com>
2020-10-15 14:00:49 +0200

    commit php

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Document</title>
+</head>
+
+<body>
+  <h1>En ce moment</h1>
+  <strong>Cliquer sur une couleur</strong>
+  <ul class="cards">
+  <li>
+      <div class="card purple" onclick="handleClick('Dark Purple')"></div>
+    </li>
+    <li>
+      <div class="card blue" onclick="handleClick('Jordy Blue')"></div>
+    </li>
+    <li>
+      <div class="card red" onclick="handleClick('Heidelberg Red')"></div>
+    </li>
+    <li>
+      <div class="card inchworm" onclick="handleClick('Inchworm')"></div>
+    </li>
+    <li>
+      <div class="card feldgrau" onclick="handleClick('Feldgrau')"></div>
+    </li>
+    <li>
+      <div class="card gris" onclick="handleClick('Gris')"></div>
+    </li>
+  </ul>
+  <a href="couleur-mystere.php?color=a71d31">Couleur Mystère</a>
+  
+  <script src="./script.js"></script>
+</body>
+
+</html>
